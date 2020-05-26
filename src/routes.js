db11f884fd98e0253d2d2782c98df4ee2915e760
Matheus Vieira <38119742+vssmatheus@users.noreply.github.com>
2020-05-25 21:59:24 -0300

    icones da tab bar

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,33 @@ import {NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ListCity from './pages/list-city';
 import AddCity from './pages/add-city';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function Routes(){
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+            screenOptions={({route})=>({
+                tabBarIcon:({color, size})=>{
+                 let iconName;
+
+                 if (route.name=='Cities') {               
+                   iconName='location-city';
+
+                 }else if(route.name=='Add City'){               
+                 iconName='add';
+                }
+               /* Estilização do icone */
+                return <MaterialIcons name={iconName} size={28} color={color} />
+                }
+               })}
+               tabBarOptions={{
+                activeTintColor: '#7159c1',
+                inactiveTintColor: 'gray',
+                }}
+               >
                 <Tab.Screen name="Cities" component={ListCity} />
                 <Tab.Screen name="Add City" component={AddCity} />
             </Tab.Navigator>
