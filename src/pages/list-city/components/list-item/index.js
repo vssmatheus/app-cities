6118c05ec728c229c330e19b593c13ba37d15025
Matheus Vieira <38119742+vssmatheus@users.noreply.github.com>
2020-05-27 21:43:40 -0300

    fetch api

--- src/pages/list-city/components/list-item/index.js
+++ src/pages/list-city/components/list-item/index.js
@@ -7,7 +7,7 @@ export default function ListItem({nomeCidade,nomePais}){
     return(
         <View style={styles.citiesList}>
             <TouchableOpacity style={styles.itemList}>
-                <Text style={styles.nomeCidade}>{nomeCidade.cidade} - {nomePais.pais} </Text>
+                <Text style={styles.nomeCidade}>{nomeCidade.nomeCidade} - {nomePais.nomePais} </Text>
                 <AntDesign name="right" size={15} color="gray" style={{alignSelf:'flex-end'}}/>
             </TouchableOpacity>            
         </View>
--- src/pages/list-city/index.js
+++ src/pages/list-city/index.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import { Image, View, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, Image, View, FlatList, ActivityIndicator } from 'react-native';
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 import ListItem from './components/list-item';
+import api from '../../services/api';
 
 export default function ListCity() {
 
-  const cidades = [
-    {id: '1', cidade: 'Maurilandia', pais:'Brasil'},
-    {id: '2', cidade: 'Santa Helena', pais:'Brasil'},
-    {id: '3', cidade: 'rio Verde', pais:'Brasil'},
-    {id: '4', cidade: 'São Paulo', pais:'Brasil'},
-    {id: '5', cidade: 'Toronto', pais:'Canada'},
-    {id: '6', cidade: 'Londres', pais:'Inglaterra'},
+  const [cidades, setCidades] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
-  ];
+  async function listarCidade(){
+    
+    /* api json esta na pasta api cidades.json */
+    const load = await fetch('https://192.168.1000.5:3000/cidades', {
+      body: JSON.stringify({
+        nomeCidade: this.cidades.nomeCidade,
+        nomePais: this.cidades.nomePais,      
+      }),
+      method: "GET",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => response.json())
+    .then((json) => setCidades(json.cidades))
+    .catch((error) => console.error(error))
+    .finally(() => setLoading(false));
+  } 
+
+  useEffect(() => {
+    listarCidade();
+  }, []);
+
     return (
       <View style={styles.container}>
         <View styles={styles.header}>
             <Image style={{alignSelf: "center"}} source={logoImg}/>
         </View>
 
-        <View style={styles.lista}>
+        <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator size="large" color="#ffff00"/> : (
+        <FlatList
+          data={cidades}
+          keyExtractor={cidade => String(cidade.id)}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item: cidade }) => (
+            <Text>{cidade.nomeCidade} - {cidade.nomePais}</Text>
+          )}
+        />
+      )}
+    </View>
+        {/* <View style={styles.lista}>
           <FlatList
             data={cidades}
             keyExtractor={item => item.id}
@@ -33,10 +64,10 @@ export default function ListCity() {
             )}
             ItemSeparatorComponent={ () => <Separator/>}
           />
-        </View>
+        </View> */}
       </View>
     );
 }
-/* linha separator */
+
 const Separator = () => <View style={{flex:1, height: 1, width:'97%', backgroundColor: '#ddd', alignSelf:"center"}}></View>
 
